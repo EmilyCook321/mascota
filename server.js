d62837ec22c6ee303517e506b3a5c680334fc2ab
EmilyCook321 <emilyacook16@gmail.com>
2020-09-09 12:03:52 +0200

    Add storybook attempt 3

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, "client/build")));
+app.use(
+  "/storybook",
+  express.static(path.join(__dirname, "client/storybook-static"))
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
