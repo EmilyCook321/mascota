2a872f09344c9671083a00a1c0bdc340fbd7e5c0
EmilyCook321 <emilyacook16@gmail.com>
2020-09-17 16:51:06 +0200

    Create fake api and test output in consolein App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { getQuestions } from "./api/getQuestions";
 
 function App() {
+  async function test() {
+    await getQuestions();
+  }
+  test();
   return (
     <div>
       <h1>Mascota</h1>
