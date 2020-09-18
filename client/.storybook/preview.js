2ad80e2100f6b81bc9cff57b63912939bf3681fa
EmilyCook321 <emilyacook16@gmail.com>
2020-09-18 10:37:02 +0200

    Enter App Decorater to preview.js, create Header/Header stories files

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,3 +1,9 @@
+import React from "react";
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  layout: "fullscreen",
 };
+const AppDecorator = (Story, context) => {
+  return <Story {...context} />;
+};
+export const decorators = [AppDecorator];
