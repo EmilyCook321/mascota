8a64b348829ffe941da3a752b2bfbbc7c1bce748
EmilyCook321 <emilyacook16@gmail.com>
2020-09-23 15:50:13 +0200

    App shows the correct answer when correct number is selected

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 // import GlobalStyles from "./GlobalStyles";
 import { getQuestions } from "./api/getQuestions";
+import NumberSelector from "./components/NumberSelector";
 import QuestionBubble from "./components/QuestionBubble";
 
 function App() {
   const [questions, setQuestions] = useState(null);
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     async function fetchQuestions() {
@@ -18,8 +20,17 @@ function App() {
     <div>
       <h1>Mascota</h1>
       {questions &&
-        questions.map(({ question, id }) => (
-          <QuestionBubble question={question} key={id} />
+        questions.map(({ question, id, answer }) => (
+          <>
+            <QuestionBubble question={question} key={id} />
+
+            <NumberSelector
+              increment={() => setCount(count + 1)}
+              decrement={() => setCount(count - 1)}
+              number={count}
+            />
+            {answer === count && <h2>The answer is correct!</h2>}
+          </>
         ))}
     </div>
   );
--- client/src/components/NumberSelector.js
+++ client/src/components/NumberSelector.js
@@ -1,25 +1,16 @@
-import React, { useState } from "react";
-// import PropTypes from "prop-types";
+import React from "react";
+import PropTypes from "prop-types";
 
-export default function NumberSelector() {
-  const [count, setCount] = useState(0);
-
-  function increment() {
-    setCount((prevCount) => prevCount + 1);
-    // console.log("increment");
-  }
-
-  function decrement() {
-    // console.log("decrement");
-    setCount((prevCount) => prevCount - 1);
-  }
-
+export default function NumberSelector(props) {
   return (
     <div>
-      <button onClick={increment}>+</button>
-      <span>{count}</span>
-      {/* <div>0</div> */}
-      <button onClick={decrement}>-</button>
+      <button onClick={props.increment}>+</button>
+      <span>{props.number}</span>
+      <button onClick={props.decrement}>-</button>
     </div>
   );
 }
+
+NumberSelector.propTypes = { increment: PropTypes.func };
+NumberSelector.propTypes = { decrement: PropTypes.func };
+NumberSelector.propTypes = { number: PropTypes.number };
