e74e85edb376fd3b5fa8ec47f6aded60bc61a460
EmilyCook321 <emilyacook16@gmail.com>
2020-09-23 21:32:08 +0200

    Add further details to global styles and and a background image

--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -14,16 +14,53 @@ const GlobalStyles = () => {
         }
         :root {
           --font-montserrat: "Montserrat-Medium";
-          --color-purple: #9e38d5;
-          --color-orange: #d4561b;
-          --color-blue: #4e7fcd;
-          --color-white: #ffffff;
+          --font-color-white: #ffffff;
+          --font-color-orange: #d4561b;
+          --wrong-answer-red: #ff3371;
+          --answer-bubble-blue: #4e7fcd;
+          --correct-answer-green: #33ffbb;
+          --base-bg-purple: #9e38d5;
+          --submit-btn-color: #4e7fcd;
+          --answer-bubble-color: #4e7fcd;
+          --heart-counter-color: #ff33b2;
         }
         html {
           @font-face {
             font-family: "Montserrat", sans-serif;
             src: url(${Montserrat}) format("ttf");
+            background-image: url("hexagon_background.jpg");
           }
+          font-family: "Montserrat";
+          height: 100vh;
+          width: 100vw;
+        }
+        body {
+          background-color: #9e38d5;
+          margin: 0;
+          padding: 0;
+        }
+
+        h1 {
+          font-family: "Montserrat";
+          font-size: 30px;
+          color: var (--font-color-white);
+        }
+
+        h2 {
+          font-family: "Montserrat";
+          font-size: 20px;
+          color: var(--font-color-white);
+        }
+
+        p {
+          color: var(--font-color-orange);
+          font-size: 15px;
+        }
+
+        button {
+          font-family: "Montserrat";
+          font-size: 16px;
+          background-color: #4e7fcd;
         }
       `}
     />
