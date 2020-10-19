864678f45f7e68149b440c9e0f40679262e8ccd9
EmilyCook321 <emilyacook16@gmail.com>
2020-10-19 14:55:37 +0200

    Add select color to Human button

--- client/src/components/HumanButton.js
+++ client/src/components/HumanButton.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styled from "@emotion/styled";
 import Human from "../assets/icons/human.svg";
 
 function SelectHuman() {
-  return <Button></Button>;
+  const [checked, setChecked] = useState(false);
+  function handleChange() {
+    setChecked(!checked);
+  }
+  return <Button value={checked} onClick={() => handleChange()}></Button>;
 }
 
 export default SelectHuman;
@@ -19,6 +23,7 @@ const Button = styled.button`
   background-image: url(${Human});
   background-repeat: no-repeat;
   background-size: auto;
+  background-color: ${({ value }) => (value ? "orange" : "--button-blue")};
   border: none;
   width: 80px;
   height: 80px;
